3df350ef72dfc9a121daecb32b7c94201cbdf3ef
Jeff Tian <jefftian@hcdlearning.com>
2018-01-10 16:17:28 +0800

    wechat login UI

---
+++ src/WechatLogin.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Container} from 'semantic-ui-react';
+
+export default class WechatLogin extends Component {
+    render() {
+        return (
+            <Container>
+                <p>Hello</p>
+            </Container>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import {browserHistory, Route, Router} from 'react-router';
+import WechatLogin from "./WechatLogin";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/wechat-login" component={WechatLogin}/>
+</Router>, document.getElementById('root'));
 registerServiceWorker();
