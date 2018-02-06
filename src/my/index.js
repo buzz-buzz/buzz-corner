6d5e0296310798828d2d625d56ec6f6e30983d86
hank <13061710755@sina.cn>
2018-02-06 14:59:46 +0800

    profile step 2.

--- src/my/index.js
+++ src/my/index.js
@@ -9,22 +9,56 @@ class Homepage extends Component {
 
         this.state = {
             step: 1,
-            parents_name: 'aa',
-            phone: '123'
+            profile: {
+                parents_name: '',
+                phone: '',
+                student_en_name: '',
+                city: '',
+                date_of_birth: '',
+                gender: ''
+            },
+            profile_title: '仅用于课程学习相关通知与服务'
         };
 
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.submit = this.submit.bind(this);
+        this.handleGender = this.handleGender.bind(this);
     }
 
-    handlePhoneChange(event) {
-        console.log(event.target);
-        this.setState({phone: event.target.value});
+    handleGender(event){
+        let clonedProfile = Object.assign({}, this.state.profile);
+
+        clonedProfile.gender =  event.target.value;
+        this.setState({profile: clonedProfile});
     }
 
-    handleNameChange(event) {
-        console.log(event.target);
-        this.setState({parents_name: event.target.value});
+    handleChange(event) {
+        let clonedProfile = Object.assign({}, this.state.profile);
+
+        clonedProfile[event.target.name] =  event.target.value;
+        this.setState({profile: clonedProfile});
+    }
+
+    async submit() {
+        try {
+            if(this.state.step < 4){
+                let newStep = this.state.step +1;
+                let newTitle = newStep === 2 ? '用于平台中呈现孩子的基本资料' : (newStep === 3 ? '用于匹配相似兴趣爱好的话题小组' : '建立孩子的语言档案');
+                this.setState({
+                    step: newStep,
+                    profile_title: newTitle
+                });
+            }else{
+                //done
+                console.log('用户填写完毕');
+                console.log(this.state.profile);
+            }
+
+            //this.setState({modal: true, message: Resources.getInstance().saveSuccess});
+        } catch (ex) {
+            console.error(ex);
+            //this.setState({modal: true, message: ex.message || Resources.getInstance().saveFailed});
+        }
     }
 
     render() {
@@ -62,27 +96,60 @@ class Homepage extends Component {
                     </div>
                 </div>
                 <Form>
-                    <h3 className="profile-title">仅用于课程学习相关通知与服务</h3>
-                    <div className="parents-name">
-                        <input type="text"  placeholder='家长姓名' style={{width: '100%'}}
-                               value={this.state.parents_name}
-                               onChange={this.handleNameChange}
-                               name='parents_name' />
-                    </div>
-                    <div className="phone-number">
-                        <Button>中国(+86)</Button>
-                        <input type="text" style={{width: '60%'}}
-                               value={this.state.phone}
-                               onChange={this.handlePhoneChange}
-                               name='phone'/>
-                    </div>
-                    <div className="check-number">
-                        <input type="text" style={{width: '60%'}}/>
-                        <Button>获取验证码</Button>
-                    </div>
+                    <h3 className="profile-title">{this.state.profile_title}</h3>
+                    {
+                        this.state.step === 1 ?
+                            (
+                                <div>
+                                    <div className="parents-name">
+                                        <input type="text"  placeholder='家长姓名' style={{width: '100%'}}
+                                               value={this.state.profile.parents_name}
+                                               onChange={this.handleChange}
+                                               name='parents_name' />
+                                    </div>
+                                    <div className="phone-number">
+                                        <Button>中国(+86)</Button>
+                                        <input type="text" style={{width: '60%'}}
+                                               value={this.state.profile.phone}
+                                               onChange={this.handleChange}
+                                               name='phone'/>
+                                    </div>
+                                    <div className="check-number">
+                                        <input type="text" style={{width: '60%'}}/>
+                                        <Button>获取验证码</Button>
+                                    </div>
+                                </div>
+                            ) : (
+                                this.state.step === 2 ? (
+                                    <div>
+                                        <div className="parents-name">
+                                            <input type="text"  placeholder='学生英文名' style={{width: '100%'}}
+                                                   value={this.state.profile.student_en_name}
+                                                   onChange={this.handleChange}
+                                                   name='student_en_name' />
+                                        </div>
+                                        <div className="gender">
+                                            <Button name="gender" value="male" onClick={this.handleGender} style={{border: this.state.profile.gender === 'male' ? '1px solid #f7b52a' : ''}} >男</Button>
+                                            <Button name="gender" value="female" onClick={this.handleGender} style={{border: this.state.profile.gender === 'female' ? '1px solid #f7b52a' : ''}}>女</Button>
+                                        </div>
+                                        <Form.Group widths='equal'>
+                                            <Form.Input value={this.state.profile.date_of_birth} type="date" onChange={this.handleChange} name='date_of_birth' />
+                                        </Form.Group>
+                                        <div className="parents-name">
+                                            <input type="text"  placeholder='所在城市' style={{width: '100%'}}
+                                                   value={this.state.profile.city}
+                                                   onChange={this.handleChange}
+                                                   name='city' />
+                                        </div>
+                                    </div>
+                                    ): (
+                                        <div></div>
+                                    )
+                            )
+                    }
                     <Form.Group widths='equal'>
-                        <Form.Field control={Button} content='继续'
-                                    style={{margin: '2em auto', width: '100%', color: 'rgba(0,0,0,.6)', background: '#eee', height: '4em', letterSpacing: '2px', fontWeight: 'normal', borderRadius: '30px'}}/>
+                        <Form.Field control={Button} content={this.state.step < 4 ? '继续' : '完成'} disabled={this.state.step === 1 ? (!this.state.profile.phone || !this.state.profile.parents_name) : (this.state.step === 2 ? (!this.state.profile.student_en_name || !this.state.profile.date_of_birth || !this.state.profile.city || !this.state.profile.gender): false)}
+                                    style={{margin: '2em auto', width: '100%', color: 'rgba(0,0,0,.6)', backgroundColor: '#f7b52a', height: '4em', letterSpacing: '2px', fontWeight: 'normal', borderRadius: '30px'}} onClick={this.submit} />
                     </Form.Group>
                 </Form>
                 <br/>
