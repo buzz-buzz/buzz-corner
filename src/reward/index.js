99c8bd122a37131969a2f7370f25d92ee75d773e
hank <13061710755@sina.cn>
2018-02-24 15:04:05 +0800

    reward page

--- src/reward/index.js
+++ src/reward/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Form, Button, Icon} from 'semantic-ui-react';
+import {Icon} from 'semantic-ui-react';
 import { browserHistory } from 'react-router';
 import CurrentUser from "../membership/user";
 import ServiceProxy from '../service-proxy';
@@ -25,6 +25,38 @@ class Reward extends Component {
                         <div>
                             <img src="http://resource.buzzbuzzenglish.com/new_buzz_logo.png"/>
                         </div>
+                    </div>
+                </div>
+                <div className="my-badge">
+                    <div className="badge-title">
+                        <p>My badge</p>
+                    </div>
+                    <div className="badge">
+                        <div className="blue-diamond">
+                            <Icon name='diamond' />
+                            <p>Blue</p>
+                            <p>Diamond</p>
+                        </div>
+                        <div className="red-diamond">
+                            <Icon name='diamond' />
+                            <p>Red</p>
+                            <p>Diamond</p>
+                        </div>
+                        <div className="yellow-diamond">
+                            <Icon name='diamond' />
+                            <p>Yellow</p>
+                            <p>Diamond</p>
+                        </div>
+                    </div>
+                    <div className="badge-rule">
+                        <p>查看获取规则</p>
+                    </div>
+                </div>
+                <div className="miles">
+                    <div className="title">BuzzBuzz Miles</div>
+                    <div className="buzz-miles">
+                        <img src="https://resource.buzzbuzzenglish.com/FpfgA6nojLQAcoXjEv7sHfrNlOVd"/>
+                        <span>0</span>
                     </div>
                 </div>
                 <Footer />
