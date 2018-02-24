7aa7635fc91ce919a4538d85da37678d24e5a36a
hank <13061710755@sina.cn>
2018-02-24 11:45:13 +0800

    button background linear.

--- src/classDetail/index.js
+++ src/classDetail/index.js
@@ -14,7 +14,7 @@ class classDetail extends Component {
                 topic: 'sing a New song',
                 time: '2018-2-1 friday',
                 partners: [1, 2, 3],
-                status: 1
+                status: 2
             }
         };
 
@@ -53,7 +53,7 @@ class classDetail extends Component {
                             <p className="class-time" style={{fontSize: '.8em', color: '#aaa'}}>11:00 - 12:00</p>
                         </div>
                         <div className="booking-item-status">
-                            <p style={this.state.class_info.status === 1 ? {color: 'red'} : (this.state.class_info.status === 2 ? {color: 'rgb(106, 225, 8)'} : {color: '#aaa'})}>{this.state.class_info.status === 1 ? '待确认' : (this.state.class_info.status === 2 ? '7天后' : '已结束')}</p>
+                            <p style={this.state.class_info.status === 1 ? {color: 'red'} : (this.state.class_info.status === 2 ? {color: 'rgb(106, 225, 8)'} : {color: '#aaa'})}>{this.state.class_info.status === 1 ? '待确认' : (this.state.class_info.status === 2 ? '7天后开始' : '已结束')}</p>
                         </div>
                     </div>
                     <div className="class-partners-avatar">
@@ -68,7 +68,36 @@ class classDetail extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="class-detail-practice"></div>
+                <div className="class-detail-practice">
+                    <div className="class-detail-notice">
+                        <p>1.在课程开始前, 你可以进行话题的模拟对话训练帮助你为今天的话题做准备。</p>
+                        <p>2.下载课程必备软件ZOOM，点击<a href="http://www.zoomcloud.cn/Download" style={{color: '#f7b52a'}}>下载安装</a> 。</p>
+                    </div>
+                    <div className="class-detail-practice-content">
+                        <div className="practise-advisor">
+                            <div className="advisor-avatar">
+                                <img src="https://resource.buzzbuzzenglish.com/FpfgA6nojLQAcoXjEv7sHfrNlOVd" alt=""/>
+                            </div>
+                            <div className="advisor-word">
+                                <p>How are you?</p>
+                            </div>
+                        </div>
+                        <div className="practise-student">
+                            <div className="student-word">
+                                <p>I'm fine, thanks. and you?</p>
+                            </div>
+                            <div className="student-avatar">
+                                <img src="https://resource.buzzbuzzenglish.com/FpfgA6nojLQAcoXjEv7sHfrNlOVd" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="class-detail-button" style={this.state.class_info.status === 2 ? {} : {display: 'none'}}>
+                        <Form.Group widths='equal'>
+                            <Form.Field control={Button} onClick={this.signUp}
+                                        content='进入课堂'/>
+                        </Form.Group>
+                    </div>
+                </div>
             </div>
         );
     }
