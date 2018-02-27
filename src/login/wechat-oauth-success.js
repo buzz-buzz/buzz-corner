50976e1d4ca6ee591f43d6d5f79f20f76d7c27e4
hank <13061710755@sina.cn>
2018-02-27 16:59:45 +0800

    login success redirect

--- src/login/wechat-oauth-success.js
+++ src/login/wechat-oauth-success.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, Segment} from "semantic-ui-react";
 import ServiceProxy from '../service-proxy';
 import BuzzServiceApiErrorParser from "../common/buzz-service-api-error-parser";
+import { browserHistory } from 'react-router';
 
 export default class WechatOAuthSuccess extends React.Component {
     constructor(props) {
@@ -20,7 +21,8 @@ export default class WechatOAuthSuccess extends React.Component {
             await this.loginNewUser(ex, this.state.wechatUserInfo);
         }
 
-        //window.location.href = ;
+        //window.location.href = ;window.location.search
+        browserHistory.push('/' + window.location.search.split('=')[1]);
     }
 
     componentWillUnmount() {
@@ -92,7 +94,7 @@ export default class WechatOAuthSuccess extends React.Component {
     render() {
         return (
             <Container textAlign="center">
-                <Segment loading={this.state.loading}>
+                <Segment loading={true} style={{position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 888}}>
                     {JSON.stringify(this.state.userInfo)}
                     <p>return_url = {window.location.search}</p>
                     <p>{decodeURIComponent(this.props.params.return_url)}</p>
