d0e6df2fb7da12a1bc867e1179c9a050368bfe7c
Jefåf Tian <jeff.tian@outlook.com>
2018-03-16 16:55:30 +0800

    add qa and production cdnifying version

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var replace = require('gulp-replace');
 var gulp = require('gulp');
 
 gulp.task('cdn', function () {
+    let cdnifiedUrl = '//cdn-corner-test.buzzbuzzenglish.com/static/';
+    if (process.env.NODE_ENV === 'production') {
+        cdnifiedUrl = '//cdn-corner.buzzbuzzenglish.com/static/';
+    }
     gulp.src(['build/index.html'])
-        .pipe(replace('/static/', '//cdn-corner-test.buzzbuzzenglish.com/static/'))
+        .pipe(replace('/static/', cdnifiedUrl))
         .pipe(gulp.dest('build/'));
 });
