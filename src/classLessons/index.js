652155dcb04aa369b5482ab1332314adb57951af
hank <13061710755@sina.cn>
2018-03-16 15:54:08 +0800

    youzan 3 images

--- src/classLessons/index.js
+++ src/classLessons/index.js
@@ -18,7 +18,7 @@ class ClassLessons extends Component {
                     price: 188,
                     discount: '6%OFF',
                     before: '原价: ¥200元',
-                    img_url: '//p579tk2n2.bkt.clouddn.com/buzz-teacher.png',
+                    img_url: '//p579tk2n2.bkt.clouddn.com/banner_buzz_youzan_2.jpg',
                     youzan_url: 'https://h5.youzan.com/v2/goods/1y44iz9a3zgsa'
                 },
                 {
@@ -27,7 +27,7 @@ class ClassLessons extends Component {
                     price: 588,
                     discount: '7%OFF',
                     before: '原价: ¥600元',
-                    img_url: '//p579tk2n2.bkt.clouddn.com/buzz-teacher.png',
+                    img_url: '//p579tk2n2.bkt.clouddn.com/banner_buzz_youzan_3.jpg',
                     youzan_url: 'https://h5.youzan.com/v2/goods/3f0foz507x14q'
                 },
                 {
@@ -36,7 +36,7 @@ class ClassLessons extends Component {
                     price: 2188,
                     discount: '15%OFF',
                     before: '原价: ¥510元',
-                    img_url: '//p579tk2n2.bkt.clouddn.com/buzz-teacher.png',
+                    img_url: '//p579tk2n2.bkt.clouddn.com/banner_buzz_youzan_5.jpg',
                     youzan_url: 'https://h5.youzan.com/v2/goods/3ewr6gf9jk4fe'
                 }
             ]
