676fc650e9980c40d8fb2ad63b566cb0e2154d7b
hank <13061710755@sina.cn>
2018-03-20 16:01:06 +0800

    three imgs for /user,/developing,/home

--- src/developing/index.js
+++ src/developing/index.js
@@ -6,7 +6,9 @@ class Developing extends Component {
     render() {
         return (
             <div className="developing">
-                <div className="develop-img"></div>
+                <div className="develop-img">
+                    <img src="//p579tk2n2.bkt.clouddn.com/icon_add_time.png" alt=""/>
+                </div>
                 <div className="develop-word">
                     {Resources.getInstance().developWord}
                 </div>
