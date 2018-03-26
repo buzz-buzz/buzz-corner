a2c9f7ea7288eba0a14db3ab62517544f3fde7a1
Jefåf Tian <jeff.tian@outlook.com>
2018-03-26 12:15:51 +0800

    Add return to fix CI error

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,16 @@ gulp.task('cdn', function () {
     if (process.env.NODE_ENV === 'qa') {
         cdnifiedUrl = '//cdn-corner-test.buzzbuzzenglish.com/static/';
     }
-    gulp.src(['build/index.html'])
+
+    return gulp.src(['build/index.html'])
         .pipe(replace('/static/', cdnifiedUrl))
         .pipe(gulp.dest('build/'));
 });
 
 gulp.task('track', function () {
-  const url = `https://jic.talkingdata.com/app/h5/v1?appid=9E0813F899A5460D953190DF02F25381&vn=${pkg.name}_${process.env.NODE_ENV}&vc=${pkg.version}`
-    gulp.src(['build/index.html'])
+    const url = `https://jic.talkingdata.com/app/h5/v1?appid=9E0813F899A5460D953190DF02F25381&vn=${pkg.name}_${process.env.NODE_ENV}&vc=${pkg.version}`
+    
+    return gulp.src(['build/index.html'])
         .pipe(replace('track_script_placeholder', url))
         .pipe(gulp.dest('build/'));
 });
