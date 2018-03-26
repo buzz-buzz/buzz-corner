b46c829d28f36fbebd36abaabad13dbf07c08419
hank <13061710755@sina.cn>
2018-03-26 11:21:43 +0800

    track ,

--- src/common/track.js
+++ src/common/track.js
@@ -2,7 +2,7 @@ const TDAPP = window.TDAPP
 
 export default {
   // track.event('首页', '消息点击', { '消息状态': '已读', '消息类型': '好友' })
-  event(EventId，Label，MapKv){
-    TDAPP && TDAPP.onEvent (EventId，Label，MapKv)
+  event(EventId, Label, MapKv){
+    TDAPP && TDAPP.onEvent (EventId, Label, MapKv)
   }
 }
