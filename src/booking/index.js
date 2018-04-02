b40b7e2d8466d4c84b8525de1357792d22d27bfe
hank <13061710755@sina.cn>
2018-04-02 15:29:25 +0800

    booking page:

---
+++ src/booking/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Resources from '../resources';
+import Track from "../common/track";
+import './index.css';
+
+class Booking extends Component {
+    constructor() {
+        super();
+
+        this.back = this.back.bind(this);
+    }
+
+    back() {
+        Track.event('预约_预约页面返回');
+
+        window.history.back();
+    }
+
+    componentDidMount() {
+        Track.event('预约_预约页面展示');
+    }
+
+    render() {
+        return (
+            <div className="booking-page">
+
+            </div>
+        );
+    }
+}
+
+export default Booking;
