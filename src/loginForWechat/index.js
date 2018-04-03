89e988676c433680887d712d9f825dc3ecf38133
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-03 18:45:04 +0800

    allow select role even in wechat login scenario

--- src/loginForWechat/index.js
+++ src/loginForWechat/index.js
@@ -2,31 +2,37 @@ import React, {Component} from 'react';
 import Resources from '../resources';
 import Track from "../common/track";
 import './index.css';
+import {MemberType} from "../membership/member-type";
+import URLHelper from "../common/url-helper";
 
 class WechatLogin extends Component {
     constructor() {
         super();
 
         this.state = {
-            step: 1
+            step: 1,
+            role: MemberType.Student
         };
 
-        this.signUp = this.signUp.bind(this);
+        this.signInViaWechat = this.signInViaWechat.bind(this);
     }
 
-    signUp() {
+    signInViaWechat() {
         Track.event('登录页面_点击微信登录按钮');
 
         if (!/MicroMessenger/.test(navigator.userAgent)) {
             alert('请在微信浏览器中使用微信登录方式');
             return;
         } else {
-            window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx370ed9dea414747f&redirect_uri=http%3A%2F%2Fcorner.buzzbuzzenglish.com%2Fwechat%2Foauth%2Fredirect%2F${(btoa(window.location.origin))}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`;
+            window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx370ed9dea414747f&redirect_uri=http%3A%2F%2Fcorner.buzzbuzzenglish.com%2Fwechat%2Foauth%2Fredirect%2F${btoa(window.location.origin)}%2F${btoa(window.location.search)}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`;
         }
     }
 
     componentDidMount() {
         Track.event('登录页面_中方微信登录页面');
+        this.setState({
+            role: URLHelper.getSearchParam(window.location.search, 'role')
+        })
     }
 
     render() {
@@ -41,7 +47,7 @@ class WechatLogin extends Component {
                         <p>{Resources.getInstance().loginByWechatIntroduction}</p>
                     </div>
                 </div>
-                <div className="login-wechat-btn" onClick={this.signUp}>
+                <div className="login-wechat-btn" onClick={this.signInViaWechat}>
                     <div>
                         <img src="//resource.buzzbuzzenglish.com/image/buzz-corner/button_WeChat.png" alt="loading..."/>
                         <p>{Resources.getInstance().loginByWechatInfo}</p>
