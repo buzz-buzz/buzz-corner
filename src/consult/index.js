a79da029fe50c9eba771d3e2c986f05ce9884330
hank <13061710755@sina.cn>
2018-04-04 17:22:50 +0800

    school name

--- src/consult/index.js
+++ src/consult/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Resources from '../resources';
 import Track from "../common/track";
+import CurrentUser from "../membership/user";
+import {MemberType} from "../membership/member-type";
 import './index.css';
 
 class Consult extends Component {
@@ -16,8 +18,12 @@ class Consult extends Component {
         window.history.back();
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         Track.event('顾问_顾问页面展示');
+
+        let profile = await CurrentUser.getProfile(true);
+
+        this.setState({role: profile.role});
     }
 
     render() {
@@ -37,10 +43,17 @@ class Consult extends Component {
                         <p>{Resources.getInstance().advisorInfo1}</p>
                         <p>{Resources.getInstance().advisorInfo2}</p>
                     </div>
-                    <div className="consult-QR-code">
-                        <img src="//p579tk2n2.bkt.clouddn.com/buzz_advisor_code.jpg" alt=""/>
-                        <p>{Resources.getInstance().advisorInfo3}</p>
-                    </div>
+                    {
+                        this.state.role === MemberType.Student ?
+                            (<div className="consult-QR-code">
+                                <img src="//p579tk2n2.bkt.clouddn.com/buzz_advisor_code.jpg" alt=""/>
+                                <p>{Resources.getInstance().advisorInfo3}</p>
+                            </div>)
+                            :
+                            (<div className="email-address">
+                                <p className="email-info">service@buzzbuzzenglish.com</p>
+                            </div>)
+                    }
                 </div>
             </div>
         );
