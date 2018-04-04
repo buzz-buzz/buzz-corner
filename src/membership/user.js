db4b182224ae0dcb9ffe745e89e992314b5effe8
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-04 12:29:41 +0800

    auto sign out if user was deleted and fill the role as per user selected.

--- src/membership/user.js
+++ src/membership/user.js
@@ -4,46 +4,53 @@ let currentUser;
 
 class User {
     constructor(userId) {
-        console.log('userId = ', userId);
         this.userId = userId;
     }
 
     static async getInstance() {
         if (!currentUser) {
-            let userData = await ServiceProxy.proxy('/user-info');
-            console.log('userData = ', userData);
+            try {
+                let userData = await ServiceProxy.proxy('/user-info');
 
-            currentUser = new User(userData.userId);
+                currentUser = new User(userData.userId);
+            } catch (ex) {
+                User.redirectToSignInPage();
+            }
         }
 
         return currentUser;
+    }
+
+    static redirectToSignInPage() {
+        console.log('redirecting...');
+        window.location.href = `/select-role?return_url=${encodeURIComponent(window.location.pathname)}${window.location.search || ''}`;
     }
 }
 
 export default class CurrentUser {
     static async getUserId() {
-        let userId = (await User.getInstance()).userId;
-        console.log('got user id = ', userId);
-        return userId;
+        return (await User.getInstance()).userId;
     }
 
     static async getProfile(force) {
         let userId = await CurrentUser.getUserId();
 
-        if (!userId) {
-            window.location.href = `/select-role?return_url=${encodeURIComponent(window.location.pathname)}${window.location.search || ''}`;
-        }
-
         if (force || !currentUser.profile) {
-            let profile = await ServiceProxy.proxyTo({
-                body: {
-                    uri: `{config.endPoints.buzzService}/api/v1/users/${userId}?t=${new Date().getTime()}`
-                }
-            }).catch(e => {
-              window.location.href = `/select-role?return_url=${encodeURIComponent(window.location.pathname)}${window.location.search || ''}`;
-            });
+            try {
+                currentUser.profile = await ServiceProxy.proxyTo({
+                    body: {
+                        uri: `{config.endPoints.buzzService}/api/v1/users/${userId}?t=${new Date().getTime()}`
+                    }
+                });
+            } catch (ex) {
+                if (ex.status === 404) {
+                    await ServiceProxy.proxy(`/sign-out`);
 
-            currentUser.profile = profile;
+                    User.redirectToSignInPage();
+                }
+
+                throw ex;
+            }
         }
 
         return currentUser.profile;
