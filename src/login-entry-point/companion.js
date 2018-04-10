6d7f7461a99b5688329590ccd44ddb203816ede2
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-10 18:59:59 +0800

    left = 0 for absolute overlay

--- src/login-entry-point/companion.js
+++ src/login-entry-point/companion.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import Resources from '../resources';
 import Track from "../common/track";
-import {Button, Divider, Form, Grid, Header, Image} from "semantic-ui-react";
+import {Divider, Form, Grid, Header, Image} from "semantic-ui-react";
 import {browserHistory} from "react-router";
 import FacebookLogin from "../login/facebook";
 import WeChatLogin from "../login/wechat";
