5665d66ff5c5de02503cdefe8459596dbf80bd20
hank <13061710755@sina.cn>
2018-04-10 15:40:19 +0800

    advisor contact

--- src/consult/index.js
+++ src/consult/index.js
@@ -27,7 +27,7 @@ class Consult extends Component {
 
         let profile = await CurrentUser.getProfile(true);
 
-        this.setState({role: profile.role});
+        this.setState({role: 'c' || profile.role});
     }
 
     render() {
@@ -39,28 +39,41 @@ class Consult extends Component {
                              src="//resource.buzzbuzzenglish.com/image/buzz-corner/icon_back.png" alt=""/>
                     </div>
                 </div>
-                <div className="consult-content">
-                    <div className="consult-avatar">
-                        <img src="//p579tk2n2.bkt.clouddn.com/buzz-teacher.png" alt=""/>
-                    </div>
-                    <div className="consult-word">
-                        <p>{Resources.getInstance().advisorInfo1}</p>
-                        <p>{Resources.getInstance().advisorInfo2}</p>
-                    </div>
-                    {
-                        this.state.role === MemberType.Student &&
-                            <div className="consult-QR-code">
-                                <img src="//p579tk2n2.bkt.clouddn.com/buzz_advisor_code.jpg" alt=""/>
-                                <p>{Resources.getInstance().advisorInfo3}</p>
-                            </div>
-                    }
-                    {
-                        this.state.role === MemberType.Companion &&
-                        <div className="email-address">
-                            <p className="email-info">peertutor@buzzbuzzenglish.com</p>
+                {
+                    this.state.role === MemberType.Student &&
+                    <div className="consult-content">
+                        <div className="consult-avatar">
+                            <img src="//p579tk2n2.bkt.clouddn.com/buzz-teacher.png" alt=""/>
                         </div>
-                    }
-                </div>
+                        <div className="consult-word">
+                            <p>{Resources.getInstance().advisorInfo1}</p>
+                            <p>{Resources.getInstance().advisorInfo2}</p>
+                        </div>
+                        <div className="consult-QR-code">
+                            <img src="//p579tk2n2.bkt.clouddn.com/buzz_advisor_code.jpg" alt=""/>
+                            <p>{Resources.getInstance().advisorInfo3}</p>
+                        </div>
+                    </div>
+                }
+                {
+                    this.state.role === MemberType.Companion &&
+                    <div className="consult-content">
+                        <div className="consult-avatar">
+                            <img src="//p579tk2n2.bkt.clouddn.com/buzz-teacher.png" alt=""/>
+                        </div>
+                        <div className="consult-word-foreign">
+                            <p>I am your private advisor</p>
+                        </div>
+                        <div className="email">
+                            <p className="email-title">1.  You can send mail to contact us</p>
+                            <p  className="email-address"><p><img style={{height: '15px'}} src="//p579tk2n2.bkt.clouddn.com/icon_mail.svg" alt=""/><span style={{verticalAlign: 'super', marginLeft: '10px'}}>peertutor@buzzbuzzenglish.com</span></p></p>
+                        </div>
+                        <p className="email-title">2. You can use wechat and <span style={{color: '#edae00'}}>scan the QR code</span></p>
+                        <div className="consult-QR-code" style={{width: '40%'}}>
+                            <img src="//p579tk2n2.bkt.clouddn.com/buzz_advisor_code.jpg" alt=""/>
+                        </div>
+                    </div>
+                }
             </div>
         );
     }
