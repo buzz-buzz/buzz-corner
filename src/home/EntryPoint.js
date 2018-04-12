81fcc828ab1cd509663e55541f29ce43c1b08727
hank <13061710755@sina.cn>
2018-04-12 11:14:14 +0800

    entry.js

--- src/home/EntryPoint.js
+++ src/home/EntryPoint.js
@@ -6,11 +6,12 @@ import URLHelper from "../common/url-helper";
 
 export default class EntryPoint extends React.Component {
     async componentDidMount() {
-        let userId = CurrentUser.getUserId();
+        let userId = await CurrentUser.getUserId();
 
         let returnUrl = URLHelper.getSearchParam(window.location.search, 'return_url');
 
         if (userId) {
+            alert(userId);
             browserHistory.push(returnUrl || '/home');
         } else {
             browserHistory.push(`/select-role?return_url=${returnUrl}`);
