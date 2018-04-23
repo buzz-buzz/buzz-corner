643c786f305dca999d880d204374bb6c14193337
hank <13061710755@sina.cn>
2018-04-23 18:04:09 +0800

    class detail UI

--- src/classDetail/classPartnersAvatars/index.js
+++ src/classDetail/classPartnersAvatars/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Resources from '../../resources';
 import './index.css';
 
 export default class ClassPartners extends React.Component {
     render() {
-        return <div className="class-partners-avatar">
-            {
-                this.props.student_avatars.length > 0 &&
-                this.props.student_avatars.map((item, index) => {
-                    return <Link key={index} to="" onClick={event => this.props.sendTrack(event, '中方头像点击')}>
-                        <img
-                            src={item.avatar || "//p579tk2n2.bkt.clouddn.com/logo-image.svg"}
-                            alt=""/>
-                    </Link>
-                })
-            }
+        return <div className="class-partners">
+            <div className="s-title">其他伙伴</div>
+            <div className="class-partners-avatar">
+                {
+                    this.props.student_avatars.length > 0 &&
+                    this.props.student_avatars.map((item, index) => {
+                        return <Link key={index} to="" onClick={event => this.props.sendTrack(event, '中方头像点击')}>
+                            <img
+                                src={item.avatar || "//p579tk2n2.bkt.clouddn.com/logo-image.svg"}
+                                alt=""/>
+                        </Link>
+                    })
+                }
+            </div>
         </div>
     }
 }
