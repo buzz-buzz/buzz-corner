6f08223cd868c50bfee76599ce1d04c35fd3786d
hank <13061710755@sina.cn>
2018-04-28 13:53:49 +0800

    button pressing style

--- src/common/commonComponent/submitButton50px/index.js
+++ src/common/commonComponent/submitButton50px/index.js
@@ -2,12 +2,36 @@ import * as React from "react";
 import './index.css';
 
 export default class Button extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pressing: false
+        };
+
+        this.touchStart = this.touchStart.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
+    }
+
+    touchStart(){
+        this.setState({
+            pressing: true
+        });
+    }
+
+    touchEnd(){
+        this.setState({
+            pressing: false
+        });
+    }
+
     render() {
         return (
-            <div className="submit-button">
+            <div className="submit-button" onTouchStart={this.touchStart} onTouchEnd={this.touchEnd}>
                 <button onClick={this.props.submit} disabled={this.props.disabled} style={ this.props.disabled === true ? {background: '#dfdfe4'} : {background: 'linear-gradient(to right, rgb(251, 218, 97) , rgb(246, 180, 12))'}}>
                     {this.props.text || 'Continue'}
                 </button>
+                <div className="press" style={this.state.pressing ? {} : {display: 'none'}}></div>
             </div>
         )
     }
