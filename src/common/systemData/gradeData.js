1b61d1a9195a892b8a3212c2265103cb29e5fb44
hank <13061710755@sina.cn>
2018-05-03 17:10:59 +0800

    simple data

---
+++ src/common/systemData/gradeData.js
@@ -0,0 +1,29 @@
+import Resources from '../../resources';
+
+export const GradeData = {
+    grade_list: [
+        {key: '1', value: '1', text: Resources.getInstance().gradeOne},
+        {key: '2', value: '2', text: Resources.getInstance().gradeTwo},
+        {key: '3', value: '3', text: Resources.getInstance().gradeThr},
+        {key: '4', value: '4', text: Resources.getInstance().gradeFou},
+        {key: '5', value: '5', text: Resources.getInstance().gradeFiv},
+        {key: '6', value: '6', text: Resources.getInstance().gradeSix},
+        {key: '7', value: '7', text: Resources.getInstance().gradeSev},
+        {key: '8', value: '8', text: Resources.getInstance().gradeEig},
+        {key: '9', value: '9', text: Resources.getInstance().gradeNin},
+    ],
+    grade_list_foreign: [
+        {key: '1', value: '1', text: 'Grade 1'},
+        {key: '2', value: '2', text: 'Grade 2'},
+        {key: '3', value: '3', text: 'Grade 3'},
+        {key: '4', value: '4', text: 'Grade 4'},
+        {key: '5', value: '5', text: 'Grade 5'},
+        {key: '6', value: '6', text: 'Grade 6'},
+        {key: '7', value: '7', text: 'Grade 7'},
+        {key: '8', value: '8', text: 'Grade 8'},
+        {key: '9', value: '9', text: 'Grade 9'},
+        {key: '10', value: '10', text: 'Grade 10'},
+        {key: '11', value: '11', text: 'Grade 11'},
+        {key: '12', value: '12', text: 'Grade 12'},
+    ]
+};
