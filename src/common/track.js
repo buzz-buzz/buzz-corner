9d5f15eda7e15c52b4f4ad8498ffa0c9b772ee69
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-05-07 13:38:00 +0800

    [+] track 增加 ua, width, height, client(tablet, phone)

--- src/common/track.js
+++ src/common/track.js
@@ -3,8 +3,15 @@ const TDAPP = window.TDAPP
 export default {
     // track.event('首页', '消息点击', { '消息状态': '已读', '消息类型': '好友' })
     event(EventId, Label, MapKv) {
+        const width = window.screen.width
+        const height = window.screen.height
+        const client = Math.min(width, height) >= 600 ? 'tablet' : 'phone'
         TDAPP && TDAPP.onEvent(EventId, Label, Object.assign({
-            url: window.location.href
+            url: window.location.href,
+            ua: window.navigator.userAgent,
+            width,
+            height,
+            client,
         }, MapKv))
     }
 }
