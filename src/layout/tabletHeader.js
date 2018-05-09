4974412631150ac81dafbb8717380bff1c0b8cf8
hank <13061710755@sina.cn>
2018-05-09 09:47:49 +0800

    big screen UI

--- src/layout/tabletHeader.js
+++ src/layout/tabletHeader.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {browserHistory} from "react-router";
 import Resources from '../resources';
 import Track from "../common/track";
 import {Link} from "react-router";
@@ -6,11 +7,21 @@ import QiniuDomain from '../common/systemData/qiniuUrl';
 import './tabletLayout.css';
 
 class TabletHeader extends Component {
+    constructor() {
+        super();
+
+        this.goHomePage = this.goHomePage.bind(this);
+    }
+
+    goHomePage(){
+        browserHistory.push('/');
+    }
+
     render() {
         return (
             <div className="tablet-header">
                 <div className="tablet-logo-left">
-                    <img src={ QiniuDomain + "/new_buzz_logo.png"} alt=""/>
+                    <img src={ QiniuDomain + "/new_buzz_logo.png"} alt="" onClick={this.goHomePage} />
                 </div>
                 <div className="tablet-middle">
                     <p>Login</p>
