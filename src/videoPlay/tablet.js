c5e625d80f31fc165ec4e5999480c72224504a26
hank <13061710755@sina.cn>
2018-05-24 09:27:47 +0800

    buzz price

--- src/videoPlay/tablet.js
+++ src/videoPlay/tablet.js
@@ -42,31 +42,6 @@ class VideoPlayTablet extends Component {
                             <img src={QiniuDomain + "/icon_play.png"} alt=""/>
                         </div>
                     </div>
-                    <div className="tablet-video-word">
-                        <div className="ad-word">
-                            <p>英美少年学伴, &nbsp;最自然的交流状态</p>
-                            <p>哈佛权威研发, &nbsp;有效提高英语口语</p>
-                            <p>百种精彩话题, &nbsp;总有属于你的独特</p>
-                        </div>
-                        <div className="class-link">
-                            <div className="link-title" style={{fontSize: '17px', color: '#000'}}>立刻加入我们吧</div>
-                            <div className="content-list">
-                                <a href={ "/" || "https://h5.youzan.com/v2/goods/1y44iz9a3zgsa"}>
-                                    <div className="class-lesson-img">
-                                        <img src={QiniuDomain + "/banner_buzz_youzan_2.jpg"} alt=""/>
-                                    </div>
-                                    <div className="class-lesson-info">
-                                        <div className="lessons-title">标准学习包</div>
-                                        <div className="lessons-price">
-                                            <div className="price">280</div>
-                                            <div className="yuan">元</div>
-                                            <div className="discount">4课时</div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <TabletFooter />
             </div>
