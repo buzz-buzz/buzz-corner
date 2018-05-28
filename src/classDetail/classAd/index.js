51fa89d1e66b44872011faffb453178ec3b75f89
hank <13061710755@sina.cn>
2018-05-28 12:37:40 +0800

    download for mac

--- src/classDetail/classAd/index.js
+++ src/classDetail/classAd/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Resources from '../../resources';
+import Client from "../../common/client";
 import './index.css';
 
     export default class ClassAd extends React.Component {
@@ -10,7 +11,7 @@ import './index.css';
             <div className="ad-content">
                 <p>{Resources.getInstance().classDetailBeforeWord1}</p>
                 <p onClick={event => this.sendTrack('下载ZOOM安装')}>{Resources.getInstance().classDetailBeforeWord2}<a
-                    href="https://zoom.us/download"
+                    href={ Client.getClient() === 'phone' ? "https://zoom.us/download" : ( (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" )}
                     style={{color: '#f7b52a', textDecoration: 'underline'}}>{Resources.getInstance().classDetailBeforeWord3}</a>
                     。</p>
             </div>
