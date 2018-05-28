2a7b8f13e1a342fcfc237336f3747c40539f5432
hank <13061710755@sina.cn>
2018-05-28 11:39:15 +0800

    update zoom download url

--- src/classDetail/classAd/index.js
+++ src/classDetail/classAd/index.js
@@ -10,7 +10,7 @@ import './index.css';
             <div className="ad-content">
                 <p>{Resources.getInstance().classDetailBeforeWord1}</p>
                 <p onClick={event => this.sendTrack('下载ZOOM安装')}>{Resources.getInstance().classDetailBeforeWord2}<a
-                    href="http://wap.zoomcloud.cn/home/download"
+                    href="https://zoom.us/download"
                     style={{color: '#f7b52a', textDecoration: 'underline'}}>{Resources.getInstance().classDetailBeforeWord3}</a>
                     。</p>
             </div>
