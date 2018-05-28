ce4476807a328141badd34438c7fc4dc3a6f9a8e
hank <13061710755@sina.cn>
2018-05-28 12:39:24 +0800

    ===

--- src/classDetail/classAd/index.js
+++ src/classDetail/classAd/index.js
@@ -11,7 +11,7 @@ import './index.css';
             <div className="ad-content">
                 <p>{Resources.getInstance().classDetailBeforeWord1}</p>
                 <p onClick={event => this.sendTrack('下载ZOOM安装')}>{Resources.getInstance().classDetailBeforeWord2}<a
-                    href={ Client.getClient() === 'phone' ? "https://zoom.us/download" : ( (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" )}
+                    href={ Client.getClient() === 'phone' ? "https://zoom.us/download" : ( (navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" )}
                     style={{color: '#f7b52a', textDecoration: 'underline'}}>{Resources.getInstance().classDetailBeforeWord3}</a>
                     。</p>
             </div>
