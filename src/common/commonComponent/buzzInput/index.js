9fd22744040c160310410450b403c2773c03e151
hank <13061710755@sina.cn>
2018-06-04 16:54:48 +0800

    pressing

--- src/common/commonComponent/buzzInput/index.js
+++ src/common/commonComponent/buzzInput/index.js
@@ -6,39 +6,18 @@ export default class BuzzInput extends React.Component {
         super(props);
 
         this.state = {
-            pressing: false,
+            pressing: false
         };
-
-        this.touchStart = this.touchStart.bind(this);
-        this.touchEnd = this.touchEnd.bind(this);
-    }
-
-    touchStart(){
-        if(!this.props.disabled){
-            this.setState({
-                pressing: true
-            });
-        }
-    }
-
-    touchEnd(){
-        if(!this.props.disabled){
-            this.setState({
-                pressing: false
-            });
-        }
     }
 
     render() {
         return (
-            <div className={ this.state.pressing ? "buzz-input pressing" : "buzz-input"}
-                 style={{height: this.props.height || '50px', width: this.props.width || '100%'}}
-                 onTouchStart={this.touchStart} onTouchEnd={this.touchEnd}
-            >
+            <div className="buzz-input"
+                 style={{height: this.props.height || '50px', width: this.props.width || '100%'}}>
                 <input type={ this.props.type || "text"}  placeholder={this.props.placeholder}
                        value={this.props.value} disabled={this.props.disabled}
                        onChange={this.props.onChange}
-                       name={this.props.name} />
+                       name={this.props.name}/>
             </div>
         )
     }
