f89c1334e8aeba3f16c61ad046e6cecc4723f1cb
hank <13061710755@sina.cn>
2018-06-14 12:05:14 +0800

    popModal text for props

--- src/common/commonComponent/popModal/index.js
+++ src/common/commonComponent/popModal/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Button50px from '../submitButton50px';
-import Resources from '../../../resources';
 import './index.css';
 
 class PopModal extends Component {
@@ -14,14 +13,14 @@ class PopModal extends Component {
             <div className="pop-modal" style={this.props.modal ? {} : {display: 'none'}} onClick={this.props.cancel}>
                 <div className="pop-content" onClick={this.stop}>
                     <div className="attention">
-                        {Resources.getInstance().popTitle}
+                        {this.props.title}
                     </div>
                     <div className="attention-info">
-                        {Resources.getInstance().popInfo}
+                        {this.props.info}
                     </div>
                     <div className="chose-buttons">
-                        <Button50px submit={this.props.cancel} text={Resources.getInstance().popCancel} />
-                        <p onClick={this.props.sure}>{Resources.getInstance().popSure}</p>
+                        <Button50px submit={this.props.cancel} text={this.props.cancelText} />
+                        <p onClick={this.props.sure}>{this.props.sureText}</p>
                     </div>
                 </div>
             </div>
