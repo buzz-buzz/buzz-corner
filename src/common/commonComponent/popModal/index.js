c8242f31114ccfc1fdceea0087d7f5a48c4e591d
hank <13061710755@sina.cn>
2018-06-14 10:30:21 +0800

    pop modal

---
+++ src/common/commonComponent/popModal/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class PopModal extends Component {
+    render(){
+        return (
+            <div className="pop-modal">
+                <div className="pop-content">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PopModal;
