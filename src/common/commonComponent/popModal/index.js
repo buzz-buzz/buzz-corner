c0fbe88bfea447fb504ca803b51d3a70f2320f61
hank <13061710755@sina.cn>
2018-06-14 11:59:55 +0800

    popModal

--- src/common/commonComponent/popModal/index.js
+++ src/common/commonComponent/popModal/index.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
+import Button50px from '../submitButton50px';
+import Resources from '../../../resources';
+import './index.css';
 
 class PopModal extends Component {
+    stop(e){
+        e.stopPropagation();
+        e.nativeEvent.stopImmediatePropagation();
+    }
+
     render(){
         return (
-            <div className="pop-modal">
-                <div className="pop-content">
-
+            <div className="pop-modal" style={this.props.modal ? {} : {display: 'none'}} onClick={this.props.cancel}>
+                <div className="pop-content" onClick={this.stop}>
+                    <div className="attention">
+                        {Resources.getInstance().popTitle}
+                    </div>
+                    <div className="attention-info">
+                        {Resources.getInstance().popInfo}
+                    </div>
+                    <div className="chose-buttons">
+                        <Button50px submit={this.props.cancel} text={Resources.getInstance().popCancel} />
+                        <p onClick={this.props.sure}>{Resources.getInstance().popSure}</p>
+                    </div>
                 </div>
             </div>
         )
