e170838e41a0a5dbbba0875ab643f40fdf59564c
hank <13061710755@sina.cn>
2018-06-14 17:50:29 +0800

    UI

--- src/languageResources/en-us.js
+++ src/languageResources/en-us.js
@@ -327,7 +327,8 @@ export const popInfo = 'Do you want to sign out of this account?';
 export const popCancel = 'Cancel';
 export const popSure = 'Sure';
 
-export const popUserUpdateAccountInfo = 'The binding phone/e-mail has been set up with an account and password. If the phone/e-mail changed, the new phone/e-mail will be used as the login account automatically.';
+export const popUserUpdateAccountInfo = 'The binding phone number has been set up with an account and password. If the phone changed, the new phone number will be used as the login account automatically.';
+export const popUserUpdateAccountInfoEmail = 'The binding e-mail has been set up with an account and password. If the e-mail changed, the new e-mail address will be used as the login account automatically.';
 
 export function signInWith(method) {
     return (
