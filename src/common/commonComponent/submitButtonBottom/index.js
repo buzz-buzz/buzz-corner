0639a0a7a572d9cc9e8d832e8499cfd2336bc5e3
hank <13061710755@sina.cn>
2018-06-19 17:09:24 +0800

    buttom style

---
+++ src/common/commonComponent/submitButtonBottom/index.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import './index.css';
+
+export default class Button extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pressing: false,
+        };
+
+        this.touchStart = this.touchStart.bind(this);
+        this.touchEnd = this.touchEnd.bind(this);
+    }
+
+    touchStart(){
+        if(!this.props.disabled){
+            this.setState({
+                pressing: true
+            });
+        }
+    }
+
+    touchEnd(){
+        if(!this.props.disabled){
+            this.setState({
+                pressing: false
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="submit-button-bottom">
+                <button className={this.state.pressing ? 'pressing' : ''}
+                        onClick={this.props.submit} onTouchStart={this.touchStart}
+                        onTouchEnd={this.touchEnd} disabled={this.props.disabled}
+                        style={ this.props.disabled ? {background: '#dfdfe4'} : {background: 'linear-gradient(to right, rgb(251, 218, 97) , rgb(246, 180, 12))'}}>
+                    {this.props.text || 'Continue'}
+                </button>
+            </div>
+        )
+    }
+}
