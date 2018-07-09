113e72eeb8a080d9b3261fc0728cf9beaf8f34e8
hank <13061710755@sina.cn>
2018-07-09 09:40:38 +0800

    help-center UI

--- src/helpCenter/index.js
+++ src/helpCenter/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {browserHistory} from 'react-router';
+import ServiceProxy from '../service-proxy';
 import HeaderWithBack from '../common/commonComponent/headerWithBack';
+
 import './index.css';
 
 export default class HelpCenter extends React.Component{
-    componentWillMount(){
+    async componentWillMount(){
         //get data from service
+        await this.updateFaq('student_index');
+    }
+
+    async updateFaq(faq_id){
+        let updatedFaq = await ServiceProxy.proxyTo({
+            body: {
+                uri: `{config.endPoints.buzzService}/api/v1/faq/${faq_id}`
+            }
+        });
+
+        this.setState({
+            faq: updatedFaq
+        });
     }
 
     back(){
@@ -20,6 +35,16 @@ export default class HelpCenter extends React.Component{
         return (
             <div className="help-center">
                 <HeaderWithBack goBack={this.back} title='help center' />
+                <div className="help-list">
+                    <div className="help-title">常见问题</div>
+                    <div className="help-item">
+                        <div className="item-name">如何上课</div>
+                        <div className="item-btn">
+                            <span className="btn-word">支持mac</span>
+                            <i className="icon-icon_back_down"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
