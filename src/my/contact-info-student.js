672d5ad1c4d18becfb572e324888ef3e1e77adbd
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-21 18:32:17 +0800

    save mobile number with country code

--- src/my/contact-info-student.js
+++ src/my/contact-info-student.js
@@ -5,7 +5,8 @@ import BuzzInput from "../common/commonComponent/buzzInput";
 import PhoneNumber from './phone-number';
 
 export default (props) => (<div className="form-content">
-    <BuzzInput type="text" placeholder={Resources.getInstance().profileParentsName}
+    <BuzzInput type="text"
+               placeholder={Resources.getInstance().profileParentsName}
                value={props.profile ? props.profile.parent_name : ''}
                onChange={props.handleChange}
                name='parent_name'
@@ -13,7 +14,9 @@ export default (props) => (<div className="form-content">
     <PhoneNumber profile={props.profile} handleChange={props.handleChange}
                  code={props.code} handleCodeChange={props.handleCodeChange}
                  waitSec={props.waitSec} mobileValid={props.mobileValid}
-                 sms={props.sms}/>
+                 sms={props.sms}
+                 mobileCountry={props.mobileCountry}
+                 onCountryCodeChange={props.onCountryCodeChange}/>
     <div className="agreement" onClick={props.agreementCheck}>
         <img
             src={props.agreement === true ? QiniuDomain + "/icon_select_active.png" : QiniuDomain + "/icon_select.png"}
