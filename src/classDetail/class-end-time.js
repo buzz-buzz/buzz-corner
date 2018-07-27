09ced8ce4b31e7fefc4e0b8dd308fe87dbc0f194
hank <13061710755@sina.cn>
2018-07-27 16:48:33 +0800

    --class_end_time

--- src/classDetail/class-end-time.js
+++ src/classDetail/class-end-time.js
@@ -4,19 +4,11 @@ import moment from "moment/moment";
  * @return {string}
  */
 export default function ClassEndTime(props) {
-    let {class_id, class_start_time, class_end_time} = props.classInfo
-
-    if (!class_start_time) {
-        class_start_time = props.classInfo.start_time
-    }
+    let {class_end_time} = props.classInfo
 
     if (!class_end_time) {
         class_end_time = props.classInfo.end_time
     }
 
-    if (isFinite(class_id)) {
-        return moment(class_start_time).add(25, 'minutes').format('HH:mm')
-    } else {
-        return moment(class_end_time).format('HH:mm')
-    }
+    return moment(class_end_time).format('HH:mm');
 }
