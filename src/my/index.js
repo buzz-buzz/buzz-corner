ede0da11e9ffee4873807ae4df0e13647df3a4b3
hank <13061710755@sina.cn>
2018-07-27 16:16:34 +0800

    test - git

--- src/my/index.js
+++ src/my/index.js
@@ -394,7 +394,8 @@ class My extends Component {
                 });
             } else if (this.state.step === 3) {
                 Track.event('注册_兴趣爱好继续-中方');
-                
+
+                //
                 this.setState({loadingModal: true});
 
                 let profileData = this.validateForm();
