a87500cb95d0d3aaf73beacf7c8f6e212fe27117
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-28 17:21:41 +0800

    add padding

--- src/classDetail/classAd/index.js
+++ src/classDetail/classAd/index.js
@@ -17,33 +17,36 @@ export default class ClassAd extends React.Component {
     }
 
     showHtml(content) {
-        return <div dangerouslySetInnerHTML={{__html: content}}></div>;
+        return <div className="class-before-content"
+                    dangerouslySetInnerHTML={{__html: content}}/>;
     }
 
     render() {
         return <div className="class-info-content">
-            <div className="s-title">{Resources.getInstance().classDetailBeforeClassNotice}</div>
+            <div
+                className="s-title">{Resources.getInstance().classDetailBeforeClassNotice}</div>
             <div className="line-middle" style={{marginBottom: '20px'}}></div>
             {
                 this.props.id === 'rookie' ?
                     <div className="class-before-content">
-                        <p onClick={event => this.sendTrack('下载ZOOM安装')}>1. 请尽快下载上课必备软件, 点击<a
-                            href={ Client.getClient() === 'phone' ? "/zoom" : ( (navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ( (navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download") )}
-                            style={{
-                                color: '#f7b52a',
-                                textDecoration: 'underline'
-                            }}>下载安装</ a>。</p>
+                        <p onClick={event => this.sendTrack('下载ZOOM安装')}>1.
+                            请尽快下载上课必备软件, 点击<a
+                                href={Client.getClient() === 'phone' ? "/zoom" : ((navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ((navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download"))}
+                                style={{
+                                    color: '#f7b52a',
+                                    textDecoration: 'underline'
+                                }}>下载安装</ a>。</p>
                         <p>2. 本次为免费的入门指导课, 将由你的班主任带你进行说明与指导。</p>
                     </div>
                     :
                     (
                         this.props.role === MemberType.Student ?
                             (this.props.content && this.props.content.student_notice_zh ?
-                                this.showHtml( window.navigator.language.toLowerCase() === 'zh-cn' ? this.props.content.student_notice_zh : this.props.content.student_notice_) :
+                                this.showHtml(window.navigator.language.toLowerCase() === 'zh-cn' ? this.props.content.student_notice_zh : this.props.content.student_notice_) :
                                 <div className="class-before-content">
                                     <p>{Resources.getInstance().classDetailBeforeWord1}</p>
                                     <p onClick={event => this.sendTrack('下载ZOOM安装')}>{Resources.getInstance().classDetailBeforeWord2}<a
-                                        href={ Client.getClient() === 'phone' ? "/zoom" : ( (navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ( (navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download") )}
+                                        href={Client.getClient() === 'phone' ? "/zoom" : ((navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ((navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download"))}
                                         style={{
                                             color: '#f7b52a',
                                             textDecoration: 'underline'
@@ -52,18 +55,18 @@ export default class ClassAd extends React.Component {
                                 </div>)
                             :
                             (this.props.content && this.props.content.companion_notice_en ?
-                                this.showHtml( window.navigator.language.toLowerCase() === 'zh-cn' ? this.props.content.companion_notice_zh : this.props.content.companion_notice_en) :
+                                this.showHtml(window.navigator.language.toLowerCase() === 'zh-cn' ? this.props.content.companion_notice_zh : this.props.content.companion_notice_en) :
                                 <div className="class-before-content">
                                     <p>{Resources.getInstance().classDetailBeforeWord1}</p>
                                     <p onClick={event => this.sendTrack('下载ZOOM安装')}>{Resources.getInstance().classDetailBeforeWord2}<a
-                                        href={ Client.getClient() === 'phone' ? "/zoom" : ( (navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ( (navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download") )}
+                                        href={Client.getClient() === 'phone' ? "/zoom" : ((navigator.platform === "Mac68K") || (navigator.platform === "MacPPC") || (navigator.platform === "Macintosh") || (navigator.platform === "MacIntel") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/zoomusInstaller.pkg" : ((navigator.platform === "Win32") || (navigator.platform === "Windows") ? "https://cdn-corner.resource.buzzbuzzenglish.com/zoom/download/ZoomInstaller.exe" : "https://zoom.us/download"))}
                                         style={{
                                             color: '#f7b52a',
                                             textDecoration: 'underline'
                                         }}>{Resources.getInstance().classDetailBeforeWord3}</ a>
                                         。</p>
                                 </div>)
-                )
+                    )
             }
         </div>
     }
