e2054c8c6b7ae2cf9a42776fee5f422ff94bfc0c
hank <13061710755@sina.cn>
2018-08-01 17:01:17 +0800

    flexCourse

---
+++ src/flexCourse/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SelectDay from './selectDate';
+import './index.css';
+
+export default class FlexCourse extends React.Component{
+    render(){
+        return <SelectDay/>
+    }
+}
---
+++ src/flexCourse/selectDate/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './index.css';
+
+export default class SelectDay extends React.Component{
+    render(){
+        return (
+            <div className="select-day">
+                <div className="month-year">July 2018</div>
+                <div className="days">
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                    <div className="day-item">
+                        <div className="day-num">25</div>
+                        <div className="day-en">MON</div>
+                        <div className="day-active"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
