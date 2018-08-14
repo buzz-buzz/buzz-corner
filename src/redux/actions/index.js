71aa1f343443ad550c1644827afecd5faa48af40
hank <13061710755@sina.cn>
2018-08-14 09:38:07 +0800

    redux bannerData

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,10 @@
-export const ADD_USER = 'ADD_USER'
-export const ADD_USERS = 'ADD_USERS'
-export const CLEAR_USERS = 'CLEAR_USERS'
-export const TOAST = 'TOAST'
-export const REPLACE_CLASS_LIST = 'REPLACE_CLASS_LIST'
+export const ADD_USER = 'ADD_USER';
+export const ADD_USERS = 'ADD_USERS';
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const TOAST = 'TOAST';
+export const REPLACE_CLASS_LIST = 'REPLACE_CLASS_LIST';
+export const ADD_YUNYING_DATA = 'ADD_YUNYING_DATA';
+export const CLEAR_YUNYING_DATA = 'CLEAR_YUNYING_DATA';
 
 export function addUser(payload) {
     return {type: ADD_USER, payload}
@@ -22,4 +24,8 @@ export function toastMessage(message) {
 
 export function replaceCurrentUserClassList(classList) {
     return {type: REPLACE_CLASS_LIST, classList}
+}
+
+export function addYunYingData(yunYingData) {
+    return {type: ADD_YUNYING_DATA, yunYingData}
 }
