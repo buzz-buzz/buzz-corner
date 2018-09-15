d8c3e96eaed0f544fcb625a545ae36c272352df4
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-09-15 13:50:59 +0800

    [!] cdn

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,68 @@
 var replace = require('gulp-replace');
 var gulp = require('gulp');
+var bluebird = require('bluebird');
 var pkg = require('./package.json');
 
-// var fs = require('fs');
-// var path = require('path');
-// var qiniu = require('qiniu');
-// const config_qiniu = {
-//     ACCESS_KEY: process.env.buzz_qiniu_access_key,
-//     SECRET_KEY: process.env.buzz_qiniu_secret_key,
-//     bucket: 'buzz-corner-user-resource',
-//     url: {
-//         upload_url: 'https://upload.qiniup.com/',
-//         resources_url: 'https://buzz-corner.user.resource.buzzbuzzenglish.com/',
-//     },
-// }
-// gulp.task('upload', async () => {
-//   const mac = new qiniu.auth.digest.Mac(config_qiniu.ACCESS_KEY, config_qiniu.SECRET_KEY)
-//   const putPolicy = new qiniu.rs.PutPolicy({
-//       scope: config_qiniu.bucket,
-//   })
-//
-//   const config = new qiniu.conf.Config()
-//   // 空间对应的机房
-//   config.zone = qiniu.zone.Zone_z0
-//   // 是否使用https域名
-//   config.useHttpsDomain = true
-//   // 上传是否使用cdn加速
-//   config.useCdnDomain = true
-//
-//   const formUploader = new qiniu.form_up.FormUploader(config)
-//   const putExtra = new qiniu.form_up.PutExtra()
-//   const uploadToken = putPolicy.uploadToken(mac)
-//   console.log(fs.readdirSync(path.join(__dirname, 'build', 'static')))
-//   // formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
-//   // respBody, respInfo) {
-//   //   if (respErr) {
-//   //     throw respErr;
-//   //   }
-//   //   if (respInfo.statusCode === 200) {
-//   //     console.log(respBody);
-//   //   } else {
-//   //     console.log(respInfo.statusCode);
-//   //     console.log(respBody);
-//   //   }
-//   // });
-// });
-
-var Qiniu = require('gulp-qiniu-utils')
-
-var qiniuOptions = {
-  ak: process.env.buzz_qiniu_access_key,
-  sk: process.env.buzz_qiniu_secret_key,
-  zone: 'Zone_z0',//空间对应存储区域（华东：z0，华北：z1，华南：z2，北美：na0）
-  bucket: 'buzz-corner-resource',//七牛对应空间
-  upload: {
-    dir: './build/buzzbuzzenglish-static',//上传本地目录
-    prefix: process.env.NODE_ENV === 'qa' ? 'corner-dev/' : 'corner-prod/', //'test/',//上传时添加的前缀，可省略
-    except: /\.(map)$/ //   /\.(html|js)$/ 上传时不上传文件的正则匹配
-  },
-  remote: {
-    url: 'https://cdn-corner.resource.buzzbuzzenglish.com',//七牛空间域名
-    prefix: {
-      default: process.env.NODE_ENV === 'qa' ? 'corner-dev/' : 'corner-prod/',//七牛空间默认前缀，如果下面三个相同可省略
-      // remove: 'test/',//七牛空间删除前缀
-      // prefetch: 'test/',//七牛空间预取前缀
-      // refresh: 'test/'//七牛空间刷新前缀
-    }
-  }
+var fs = require('fs');
+var path = require('path');
+var readdir = require("recursive-readdir");
+var qiniu = require('qiniu');
+const config_qiniu = {
+    ACCESS_KEY: process.env.buzz_qiniu_access_key,
+    SECRET_KEY: process.env.buzz_qiniu_secret_key,
+    bucket: 'buzz-corner-resource',
+    url: {
+        upload_url: 'https://upload.qiniup.com/',
+        resources_url: 'https://cdn-corner.resource.buzzbuzzenglish.com',
+    },
 }
+gulp.task('upload-cdn', async cb => {
+  const mac = new qiniu.auth.digest.Mac(config_qiniu.ACCESS_KEY, config_qiniu.SECRET_KEY)
+  const putPolicy = new qiniu.rs.PutPolicy({
+      scope: config_qiniu.bucket,
+  })
 
-gulp.task('upload-cdn', function (cb) {
-  var qiniu = new Qiniu(qiniuOptions)
-  qiniu.upload()
-  .then(files=>console.log(files))
-    .then(r => cb())
-})
+  const config = new qiniu.conf.Config()
+  // 空间对应的机房
+  config.zone = qiniu.zone.Zone_z0
+  // 是否使用https域名
+  config.useHttpsDomain = true
+  // 上传是否使用cdn加速
+  config.useCdnDomain = true
+
+  const formUploader = new qiniu.form_up.FormUploader(config)
+  const putExtra = new qiniu.form_up.PutExtra()
+  const uploadToken = putPolicy.uploadToken(mac)
+  const folder = path.join(__dirname, './build/buzzbuzzenglish-static')
+  await bluebird.map(await readdir(folder,['*.map', '.DS_Store']), async localFile => {
+    return new Promise(function(resolve, reject) {
+      const prefix = process.env.NODE_ENV === 'qa' ? 'live-dev/static' : 'live/static'
+      const key = `${prefix}${localFile.replace(folder, '')}`
+      formUploader.putFile(uploadToken, key, localFile, putExtra, function(respErr,
+      respBody, respInfo) {
+        if (respErr) {
+          reject(respErr)
+        }
+        if (respInfo.statusCode === 200) {
+          console.log(key)
+          console.log(respBody)
+          resolve(respBody)
+        } else {
+          console.log(respInfo.statusCode);
+          console.log(respBody);
+          reject(new Error('upload error'))
+        }
+      });
+    });
+  })
+  cb()
+
+});
 
 gulp.task('replace-cdn', function () {
-    let cdnifiedUrl = '//cdn-corner.resource.buzzbuzzenglish.com/corner-prod/build/buzzbuzzenglish-static/';
+    let cdnifiedUrl = '//cdn-corner.resource.buzzbuzzenglish.com/live/static/';
     if (process.env.NODE_ENV === 'qa') {
-        cdnifiedUrl = '//cdn-corner.resource.buzzbuzzenglish.com/corner-dev/build/buzzbuzzenglish-static/';
+        cdnifiedUrl = '//cdn-corner.resource.buzzbuzzenglish.com/live-dev/static/';
     }
     return gulp.src('build/**/*.*')
         .pipe(replace(/\/buzzbuzzenglish-static\//g,`${cdnifiedUrl}`))
